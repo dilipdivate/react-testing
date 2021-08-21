0e7f684e1b807b7f61811616d0d93b174190c046
dilipdivate <dilip.divate@gmail.com>
2021-08-22 01:52:28 +1000

    Making changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,28 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export function replaceCamelWithSpaces(colorName: string) {
+  return colorName.replace(/\B([A-Z])\B/g, ' $1')
+}
+
 function App() {
-  const [buttonColor, setButtonColor] = useState('red');
-  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+  const [buttonColor, setButtonColor] = useState('MediumVioletRed');
+  const newButtonColor = buttonColor === 'MediumVioletRed' ? 'MidnightBlue' : 'MediumVioletRed';
+  const [disabled, setDisabled] = useState(false)
 
   return (
-    <div className="App-local-revert">
+    <div className="App">
 
-        <button style={{backgroundColor: buttonColor}} onClick={() => setButtonColor(newButtonColor)}>Change to {newButtonColor}</button>
+        <button style={{backgroundColor: disabled ? 'gray' : buttonColor}} 
+                onClick={() => setButtonColor(newButtonColor)}
+                disabled={disabled}>
+                  Change to {replaceCamelWithSpaces(newButtonColor)}</button>
+        <input type="checkbox" 
+               defaultChecked={disabled} 
+               id="disable-button-checkbox"
+               aria-checked={disabled}
+               onChange={ (e) => setDisabled(e.target.checked)}     />
+        <label htmlFor="disable-button-checkbox">Disabled Button</label>
     </div>
   );
 }
