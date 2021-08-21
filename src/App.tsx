6f5deaaaf81efb9613e6bdf33e89a76e26584a13
dilipdivate <dilip.divate@gmail.com>
2021-08-21 22:08:00 +1000

    Local

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
 
   return (
-    <div className="App-remote">
+    <div className="App-local">
         <button style={{backgroundColor: buttonColor}} onClick={() => setButtonColor(newButtonColor)}>Change to {newButtonColor}</button>
     </div>
   );
