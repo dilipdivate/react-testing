572e468a77dd6892ae1356b25a3f62a10081e0ca
dilipdivate <dilip.divate@gmail.com>
2021-08-21 20:59:55 +1000

    New changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
 
   return (
-    <div className="App-changes">
+    <div className="App">
         <button style={{backgroundColor: buttonColor}} onClick={() => setButtonColor(newButtonColor)}>Change to {newButtonColor}</button>
     </div>
   );
