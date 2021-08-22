bfab5d2b4c0543de48058c3f3cefb3f58feb9b47
dilipdivate <dilip.divate@gmail.com>
2021-08-22 21:06:20 +1000

    adding mocks

--- src/pages/summary/SummaryForm.tsx
+++ src/pages/summary/SummaryForm.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { OverlayTrigger, Popover } from 'react-bootstrap';
-// import './App.css';
+// import './App.css'; testing
 
 function SummaryForm() {
   const [tcChecked, setTcChecked] = useState(false);
